128d52e06fa2f19fcd249f7a52a3c2d632976c22
Lucas Borba <lucas_borba@outlook.com>
2022-10-21 20:57:59 -0300

    DIO Imagens em Python

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing",
+    version="0.0.1",
+    author="Lucas Oliveira de Borba",
+    author_email="lucas_borba@outlook.com",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
