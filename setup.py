02622a105c3dc477b44dc0649189606d669319f1
Lucas Borba <lucas_borba@outlook.com>
2022-10-21 22:12:15 -0300

    DIO upload Pypi

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
-    version="0.0.2",
-    author="Lucas Oliveira de Borba",
+    name="imagemDIO",
+    version="0.0.1",
+    author="oliverborba",
     author_email="lucas_borba@outlook.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="git remote add origin https://github.com/oliverborba/DIO_image_processing.git",
+    url="",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
